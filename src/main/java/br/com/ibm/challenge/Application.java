efb2f106d1d9308158e77eca23ded5691414fb98
lucasfdcampos <_83_wm_tz_>
2019-10-29 10:01:15 -0300

    Removido funcao de teste.

--- src/main/java/br/com/ibm/challenge/Application.java
+++ src/main/java/br/com/ibm/challenge/Application.java
@@ -6,21 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void calcularCedulas(int valor) {
-		int[] cedulas = {100, 50, 20, 10};
-
-		for (int i = 0; i < cedulas.length; i++) {
-			if (valor >= cedulas[i]) {
-				System.out.println(valor/cedulas[i] + " notas de " + cedulas[i]);
-				valor = valor % cedulas[i];
-			}
-		}
-		System.out.println("Sobram: " + valor);
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("calculando cedulas");
-		calcularCedulas(130);
 	}
 }
